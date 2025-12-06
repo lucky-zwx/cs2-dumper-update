6edec61cce28f2bc05b9ae1a98a468944f8c4a25
朱文骁 <34543176+lucky-zwx@users.noreply.github.com>
2025-12-06 19:12:49 +0800

    Add files via upload

--- output/pulse_system_dll.rs
+++ output/pulse_system_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-11-26 02:17:26.946799800 UTC
+// 2025-12-06 11:10:50.444418500 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
--- output/vphysics2_dll.cs
+++ output/vphysics2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-11-26 02:17:26.946799800 UTC
+// 2025-12-06 11:10:50.444418500 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: vphysics2.dll
