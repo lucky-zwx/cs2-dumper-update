7610626625feb025369caff1a533e776e97504cb
朱文骁 <34543176+lucky-zwx@users.noreply.github.com>
2025-12-10 21:06:43 +0800

    Add files via upload

--- output/vphysics2_dll.cs
+++ output/vphysics2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-12-09 08:02:57.643690400 UTC
+// 2025-12-10 13:01:15.505903200 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: vphysics2.dll
